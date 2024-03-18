bb885d8e8a10e667393ff4476d859339f086e193
s27944 <s27944@pjwstk.edu.pl>
2024-03-18 10:55:39 +0100

    Zmiana nazwy zmiennej

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -17,14 +17,14 @@ public class Program
 
     public static int Avg(int[] tab)
     {
-        int sum = 0;
+        int sumaa = 0;
         foreach (int num in tab)
         {
-            sum += num;
+            sumaa += num;
             
         }
 
-        return sum / tab.Length;
+        return sumaa / tab.Length;
     }
 
 
